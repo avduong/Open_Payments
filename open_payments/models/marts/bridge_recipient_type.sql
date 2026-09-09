95afa03d50ac953d5ecbb5366a594e4dd5fb2d5a
Alex <aduong15@wgu.edu>
2026-09-09 14:15:53 -0700

    Change plan from star schema to One Big Table model.

--- open_payments/models/marts/bridge_recipient_type.sql
+++
@@ -1,5 +0,0 @@
-select
-    recipient_profile_id,
-    recipient_type,
-    recipient_type_order
-from {{ ref('stg_open_payments') }}
--- open_payments/models/marts/dim_recipient.sql
+++
@@ -1,6 +0,0 @@
-select distinct
-    recipient_profile_id
-
-from {{ ref('stg_open_payments') }}
-
-where recipient_profile_id is not null
